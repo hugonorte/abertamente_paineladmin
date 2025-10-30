559b71cd98fe2f8554fdf8cdaef92fe0ce8ec99a
Hugo Norte <hugonorte@gmail.com>
2025-10-29 23:29:45 -0300

    chore: add initial Nuxt.js configuration file

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false,
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon'],
+  runtimeConfig: {
+        public: {
+            // Disponibiliza a URL da API para o lado do cliente (navegador)
+            apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'https://admin.abertamente.net/api'
+        }
+    }
+})
