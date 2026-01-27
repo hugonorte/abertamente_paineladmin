8a23486664934d12695c7add0e5c744bf3630e78
Hugo Norte <hugonorte@gmail.com>
2026-01-27 00:38:37 -0300

    feat: introduce author management with dedicated API endpoints and admin pages for creating and listing authors.

---
+++ app/api/author/get.ts
@@ -0,0 +1,42 @@
+import type { Author } from '~/types/models';
+const config = useRuntimeConfig()
+const apiUrl = config.public.apiBaseUrl;
+
+interface AuthorData {
+    message: string;
+    author: Author;
+}
+
+export async function fetchAuthors() {
+    const auth = useAuth()
+    const token = auth.token.value
+    if (!token) {
+        throw createError({
+            statusCode: 401,
+            statusMessage: 'O usuário não está autenticado',
+        })
+    }
+
+    try {
+        const options = {
+            method: 'GET' as 'GET',
+            credentials: 'include' as RequestCredentials,
+            headers: {} as Record<string, string>,
+        };
+
+        if (token) {
+            options.headers.Authorization = `Bearer ${token}`;
+        }
+
+        const response = await $fetch<Author[]>(`${apiUrl}/author`, options)
+
+        return response
+    }
+    catch (error) {
+        throw createError({
+            statusCode: 500,
+            statusMessage: 'Erro ao buscar autores',
+        })
+    }
+}
+
