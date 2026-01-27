f279b5e4c1034b196f4cc3944661dc320261b532
Hugo Norte <hugonorte@gmail.com>
2026-01-27 00:40:00 -0300

    feat: Implement author management with a new model, an admin listing page, a navigation link, and dynamic author selection in post creation.

---
+++ app/pages/admin/author/index.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'admin',
+})
+
+import { ref, onMounted } from 'vue'
+import type { TableColumn } from '@nuxt/ui'
+import type { Author } from '~/types/models'
+import { fetchAuthors } from '~/api/author/get'
+
+const Authors = ref<Author[]>([])
+const isLoading = ref<boolean>(false)
+const globalFilter = ref('')
+
+onMounted(async () => {
+  isLoading.value = true
+  try {
+    Authors.value = await fetchAuthors()
+  } catch (error) {
+    console.error(error)
+  } finally {
+    isLoading.value = false
+  }
+})
+
+const columns: TableColumn<Author>[] = [
+  {
+    accessorKey: 'id',
+    header: '#',
+    cell: ({ row }) => `#${row.getValue('id')}`
+  },
+  {
+    accessorKey: 'name',
+    header: 'Nome'
+  },
+  {
+    accessorKey: 'email',
+    header: 'Email'
+  }
+]
+</script>
+
+<template>
+  <UPageBody>
+      <UContainer>
+          <UPageHeader title="Autores"  />
+      </UContainer>
+      <UContainer>
+      <UCard>
+        <template #header>
+          <ULink href="/admin/author/create">
+            <UButton>Criar Novo autor</UButton>
+          </ULink>
+        </template>
+      </UCard>
+      <UAlert v-if="isLoading" color="info" icon="i-heroicons-refresh-cw" :title="'Carregando...'" />
+      <div v-else class="flex flex-col flex-1 w-full">
+        <div class="flex px-4 py-3.5 border-b border-accented">
+          <UInput v-model="globalFilter" class="max-w-sm" placeholder="Buscar..." />
+        </div>
+
+        <UTable ref="table" v-model:global-filter="globalFilter" :data="Authors" :columns="columns" />
+      </div>
+    </UContainer>
+  </UPageBody>
+</template>
