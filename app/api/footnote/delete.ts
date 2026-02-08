d10c3b7f80f8534d21d8f80e8a653e617fd2c9e8
Hugo Norte <hugonorte@gmail.com>
2026-02-08 13:23:16 -0300

    feat: Implement post status management, update post editing, and add bibliographic reference and footnote CRUD operations.

---
+++ app/api/footnote/delete.ts
@@ -0,0 +1,39 @@
+import type { Footnote } from '~/types/models';
+const config = useRuntimeConfig()
+const apiUrl = config.public.apiBaseUrl;
+
+export async function deleteFootnote(id: number) {
+    const auth = useAuth()
+    const token = auth.token.value
+    if (!token) {
+        throw createError({
+            statusCode: 401,
+            statusMessage: 'O usuário não está autenticado',
+        })
+    }
+
+    try {
+        const options = {
+            method: 'DELETE' as 'DELETE',
+            credentials: 'include' as RequestCredentials,
+            headers: {
+                'Accept': 'application/json'
+            } as Record<string, string>,
+        };
+
+        if (token) {
+            options.headers.Authorization = `Bearer ${token}`;
+        }
+
+        const response = await $fetch(`${apiUrl}/footnote/${id}`, options)
+
+        return response
+    }
+    catch (error) {
+        throw createError({
+            statusCode: 500,
+            statusMessage: 'Erro ao deletar nota de rodapé',
+        })
+    }
+}
+
