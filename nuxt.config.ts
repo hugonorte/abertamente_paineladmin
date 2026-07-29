542363279e40ef00a5340a4183c553e3208d7af5
Hugo Norte <hugonorte@gmail.com>
2026-07-29 13:37:11 -0300

    feat: add custom SVG favicon to app configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,13 @@ export default defineNuxtConfig({
   ssr: false,
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }
+      ]
+    }
+  },
   modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon','@nuxt/ui'],
   colorMode: {
     preference: 'dark',
